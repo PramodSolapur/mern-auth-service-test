ce26fe392bb66b40ab891ac1d6c8492030bc9867
PramodSolapur <psolapur5@gmail.com>
2025-08-13 13:27:09 +0530

    add tenant id foreign key in users

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { Tenant } from './Tenant'
 
 // Set  "strictPropertyInitialization": false in tasconfig.json file
 
@@ -23,4 +24,7 @@ export class User {
 
     @Column()
     role: string
+
+    @ManyToOne(() => Tenant)
+    tenant: Tenant
 }
